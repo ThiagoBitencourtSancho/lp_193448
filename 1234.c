4b30d6a7754c3cc913fd92a781f9f3498c18dc0e
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-04-28 23:13:31 -0300

    Update 1234.c

--- 1234.c
+++ 1234.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THIAGO BITENCOURT SANCHO
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 28/04/2026
+Objetivo    : SENTENCA DANCANTE
+Aprendizado : APRENDI A FAZER UMA SENTENCA DANCANTE
+-------------------------------------------------------------------------- */
 #include<stdio.h>
 #include<ctype.h>
 
