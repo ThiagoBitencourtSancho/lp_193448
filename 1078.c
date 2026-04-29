90dbc778083d1c146c529a6c2dc5f05432b20799
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-04-28 23:22:26 -0300

    Update 1078.c

--- 1078.c
+++ 1078.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THIAGO BITENCOURT SANCHO
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
+Data        : 28/04/2026
+Objetivo    : TABUADA
+Aprendizado : APRENDI A FAZER A TABUADA
+-------------------------------------------------------------------------- */
 #include<stdio.h>
 
 int main (void){
