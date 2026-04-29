8b97af54baa43f12216dce9d19be35e1705c930a
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-04-28 21:42:06 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THIAGO BITENCOURT SANCHO
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 28/04/2026
+Objetivo    : PRINTAR HELLO WORD
+Aprendizado : APRENDI A FAZER PRINTF
+-------------------------------------------------------------------------- */
 #include<stdio.h>
 
 int main(void){
