e97664c51e45f750dd4b61fbc65a839e60b065f3
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-04-28 21:44:39 -0300

    Update 1080a.c

--- 1080a.c
+++ 1080a.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THIAGO BITENCOURT SANCHO
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 28/04/2026
+Objetivo    : MOSTRAR O MAIOR DE 100 NUMEROS E SUA POSICAO.
+Aprendizado : APRENDI A LIDAR COM LAÇOS
+-------------------------------------------------------------------------- */
 #include<stdio.h>
 
 int main(void){
