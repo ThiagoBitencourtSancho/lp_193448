2a9a4c86645d78f113a9da10ed3803cd741efbfe
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-05-14 13:49:39 -0300

    Create 1534ComVetor.c

---
+++ 1534ComVetor.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main(void){
+	
+ int colunas;
+	
+ while(scanf("%d" , &colunas) != EOF){
+  char linha[colunas + 1];
+  linha[colunas] = '\0';
+  linha[0] = '1';
+  linha[colunas-1] = '2';
+  
+  for(int posicao = 1; posicao<=colunas-2; posicao++){
+   linha[posicao] = '3';
+  } 
+  
+  
+  int mudar1 = 1;
+  int mudar2 = colunas - 2;
+  
+  for(int contador = 0; contador < colunas; contador++){
+   printf("%s\n" , linha);
+   
+   
+   linha[mudar1-1] = '3';
+   linha[mudar2 + 1] = '3';
+   
+   linha[mudar1] = '1';
+   linha[mudar2] = '2';
+   
+   
+   mudar1++;
+   mudar2--;
+    
+  } 
+ }
+	
+	
+
+	
+}
