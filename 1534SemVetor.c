e5c89ff1a9bc9cc9db5d7c82b5afe36df1f985d0
ThiagoBitencourtSancho <thiago.sancho@unifesp.br>
2026-05-14 13:55:13 -0300

    Update 1534SemVetor.c

--- 1534SemVetor.c
+++ 1534SemVetor.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THIAGO BITENCOURT SANCHO
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1534
+Data        : 14/05/2026
+Objetivo    : VETOR 123
+Aprendizado : APRENDI A LIDAR VETORES
+-------------------------------------------------------------------------- */
+
 #include<stdio.h>
 
 int main(void){
